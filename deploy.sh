3ad8435f2169aa7b6b32c7562b3a2ad436814e73
=zxy <zxy1337445805@gmail.com>
2022-10-10 23:20:25 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:HIDE-A-PUMPKIN/hide-a-pumpkin.github.io.git master
+# git push -f git@github.com:HIDE-A-PUMPKIN/hide-a-pumpkin.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:HIDE-A-PUMPKIN/hide-a-pumpkin.git main:gh-pages
+git push -f git@github.com:HIDE-A-PUMPKIN/hide-a-pumpkin.git main:gh-pages
 
 cd -
